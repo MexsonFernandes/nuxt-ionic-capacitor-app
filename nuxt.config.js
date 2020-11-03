853a6a20f77924fee831dd5d97edb6d0b77f2d81
Mexson Fernandes <robomex2020@gmail.com>
2020-11-03 19:13:42 +0530

    Remove pwa nuxt module

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,12 +39,8 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    // '@nuxtjs/bulma',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content'
   ],
