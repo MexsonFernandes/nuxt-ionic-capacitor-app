0f544ba430bdf1d1511df09debaee011813b0dfb
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-28 12:30:16 +0800

    Delete web directory

--- web/plugins/capacitor.js
+++
@@ -1,7 +0,0 @@
-import { Toast } from '@capacitor/toast'
-import { Network } from '@capacitor/network'
-
-export default function ({ app }, inject) {
-  inject('toast', Toast)
-  inject('network', Network)
-}
--- web/plugins/ionic.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-// add a direct link to @ionic/core
-import { defineCustomElements as Ionic } from '@ionic/core/loader'
-import { defineCustomElements as Pwa } from '@ionic/pwa-elements/loader'
-
-Vue.use(Ionic)
-Vue.use(Pwa)
-Vue.config.ignoredElements = [/^ion-/]
